9e81d1f07402a9c4d44055fec7a2c8a5b41c12dc
gergely999 <nagygergely011@gmail.com>
2021-03-07 18:25:37 +0100

    progress added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function calculate(){
+    let price = 1350;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amount = parseInt(amountInput.value) * price;
+    
+    let showAmount = document.querySelector("span.show-amount");
+
+}
