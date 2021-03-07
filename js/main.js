c34d37136187b84b499001a8f0f13687f71b1d0c
gergely999 <nagygergely011@gmail.com>
2021-03-07 19:07:01 +0100

    form changes added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 function calculate(){
     let price = 1350;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let amount = parseInt(amountInput.value) * price;
-    
     let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
+
+    if(isNaN(amountNumber)){
+        amountNumber = 0;
+    }
+
+    if(amountNumber > 10){
+        alert("Maximum 10 terméket vásárolhat!");
+    } else if (amountNumber < 1){
+        alert("A kosár üres!");
+    } else {
+        let amount = parseInt(amountInput.value) * price;
+        showAmount.innerHTML = amount;
+    }
 
 }
